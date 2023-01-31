7e5824014d485f4166704956261e14e9a16a84c1
Civitasv <hscivitasv@gmail.com>
2023-01-31 21:32:54 +0800

    Deploy website - based on 2caf2a05ab21989c4efd0b8a7a18d884c7c54bc3

---
+++ source/friend_clz.cc
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+class FriendClass {
+private:
+  int private_data;
+
+  // this means Other is my friend,
+  // please let him have access to my private memeber
+  friend class Other;
+};
+
+class Other {
+public:
+  FriendClass clz;
+  Other() { clz.private_data = 2; }
+
+  void Print() { std::cout << "PRIVATE: " << clz.private_data; }
+
+  void RetrieveData();
+};
+
+int main() {
+  Other other;
+  other.Print();
+
+  return 0;
+}
---
+++ source/friend_func.cc
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+class FriendClass;
+
+class Another {
+public:
+  void member_function(const FriendClass &clz);
+};
+
+class FriendClass {
+public:
+  FriendClass(int private_data) : private_data(private_data) {}
+
+  // please let him have access to my private memeber
+  friend void friend_function(const FriendClass &clz);
+
+  friend void Another::member_function(const FriendClass &clz);
+
+private:
+  int private_data;
+};
+
+void friend_function(const FriendClass &clz) {
+  std::cout << clz.private_data << '\n';
+}
+
+// friend function definition
+void Another::member_function(const FriendClass &clz) {
+  std::cout << "Private Variable: " << clz.private_data << std::endl;
+}
+
+int main() {
+  friend_function(2);
+
+  Another another;
+  another.member_function(2);
+  return 0;
+}
